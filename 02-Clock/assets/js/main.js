f4fd25d42d1ad5d6c6588ee9538e3e0397bf77f8
Simo Aleksandrov <simo3003@me.com>
2018-11-17 00:54:36 +0200

    Update bezier

--- 02-Clock/assets/js/main.js
+++ 02-Clock/assets/js/main.js
@@ -13,18 +13,17 @@ function updateTime() {
   let minDeg = 0;
   let hourDeg = 0;
 
-  seconds++;
   secDeg += seconds * 6; // one second = 6deg
-
-  minutes = seconds % 60 == 0 ? minutes + 1 : minutes;
   minDeg += minutes * 6 + (seconds / 60) * 6; // one minute = 6deg
-
-  hours = minutes % 60 == 0 ? hours + 1 : hours;
   hourDeg += hours * 30 + (minutes / 60) * 30; // one hour = 30deg
-
+ 
   secondsHand.style.transform = `rotate(${secDeg}deg)`;
   minutesHand.style.transform = `rotate(${minDeg}deg)`;
   hoursHand.style.transform = `rotate(${hourDeg}deg)`;
+
+  seconds++;
+  minutes = seconds % 60 == 0 ? minutes + 1 : minutes;
+  hours = minutes % 60 == 0 && minutes != 0 ? hours + 1 : hours;
 }
 
 updateTime();
