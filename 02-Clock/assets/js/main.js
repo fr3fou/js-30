155f1af3fb2ae2c1ccc65f44b06497b8aa787a92
Simo Aleksandrov <simo3003@me.com>
2018-11-17 21:29:18 +0200

    Add 04

--- 02-Clock/assets/js/main.js
+++ 02-Clock/assets/js/main.js
@@ -16,14 +16,14 @@ function updateTime() {
   secDeg += seconds * 6; // one second = 6deg
   minDeg += minutes * 6 + (seconds / 60) * 6; // one minute = 6deg
   hourDeg += hours * 30 + (minutes / 60) * 30; // one hour = 30deg
- 
+
   secondsHand.style.transform = `rotate(${secDeg}deg)`;
   minutesHand.style.transform = `rotate(${minDeg}deg)`;
   hoursHand.style.transform = `rotate(${hourDeg}deg)`;
 
   seconds++;
   minutes = seconds % 60 == 0 ? minutes + 1 : minutes;
-  hours = minutes % 60 == 0 && minutes != 0 ? hours + 1 : hours;
+  hours = minutes % 60 == 0 && seconds & (60 == 0) ? hours + 1 : hours;
 }
 
 updateTime();
